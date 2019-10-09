63b289e4d4743d89dfad829f54d070521c2c6d5d
zeynebetul <56346728+zeynebetul@users.noreply.github.com>
2019-10-09 16:00:06 +0300

    Update HomeControllerTest.java

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello from GazGeek!");
+            .assertResponseBody("Hello from zeyneb betul !!!!!");
     }
 
     private HelloWorldResponse getRequest(String uri) {
